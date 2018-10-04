86937d2e69865e8b72e9e66d213aa72dcf46b4fe
Eugene Janusov <esycat@gmail.com>
2018-10-04 23:28:07 +1000

    'Reset State to Open when unassigned' rule should not be triggered if the issue is already resolved

--- assignee-state/reset-state-when-unassigned.js
+++ assignee-state/reset-state-when-unassigned.js
@@ -2,24 +2,24 @@ var entities = require('@jetbrains/youtrack-scripting-api/entities');
 var workflow = require('@jetbrains/youtrack-scripting-api/workflow');
 
 exports.rule = entities.Issue.onChange({
-  title: workflow.i18n('Reset State to Open when unassigned'),
-  guard: function(ctx) {
-    var issue = ctx.issue;
-    var fields = issue.fields;
-    return issue.isChanged(ctx.Assignee) && !fields.Assignee && fields.State.name != ctx.State.Open.name;
-  },
-  action: function(ctx) {
-    var issue = ctx.issue;
-    issue.fields.State = ctx.State.Open;
-    workflow.message(workflow.i18n('The issue\'s state has been reset'));
-  },
-  requirements: {
-    Assignee: {
-      type: entities.User.fieldType
+    title: workflow.i18n('Reset State to Open when unassigned'),
+    guard: function (ctx) {
+        var issue = ctx.issue;
+        var fields = issue.fields;
+        return fields.becomes(ctx.Assignee, null) && !issue.isResolved && fields.State.name != ctx.State.Open.name;
     },
-    State: {
-      type: entities.State.fieldType,
-      Open: {}
+    action: function (ctx) {
+        var issue = ctx.issue;
+        issue.fields.State = ctx.State.Open;
+        workflow.message(workflow.i18n('The issue\'s state has been reset'));
+    },
+    requirements: {
+        Assignee: {
+            type: entities.User.fieldType
+        },
+        State: {
+            type: entities.State.fieldType,
+            Open: {}
+        }
     }
-  }
 });
