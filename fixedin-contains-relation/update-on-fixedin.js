b0bb1062276afd3540c109e920a6e3c226857731
Eugene Janusov <esycat@gmail.com>
2018-10-12 00:57:10 +1100

    Code formatting
    #codestyle

--- fixedin-contains-relation/update-on-fixedin.js
+++ fixedin-contains-relation/update-on-fixedin.js
@@ -3,14 +3,14 @@ var workflow = require('@jetbrains/youtrack-scripting-api/workflow');
 var utils = require('./utils');
 
 exports.rule = entities.Issue.onChange({
-  title: workflow.i18n('Update when marked as “Fixed in”'),
-  guard: function(ctx) {
-    return ctx.issue.links['fixed in'].added.isNotEmpty();
-  },
-  action: function(ctx) {
-    var issue = ctx.issue;
-    var fixedIn = issue.links['fixed in'].added.first();
+    title: workflow.i18n('Update when marked as “Fixed in”'),
+    guard: function (ctx) {
+        return ctx.issue.links['fixed in'].added.isNotEmpty();
+    },
+    action: function (ctx) {
+        var issue = ctx.issue;
+        var fixedIn = issue.links['fixed in'].added.first();
 
-    utils.updateFrom(issue, fixedIn);
-  }
+        utils.updateFrom(issue, fixedIn);
+    }
 });
